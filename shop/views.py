9d6b2f6402d9e04477cadad5c049303290c72132
Maxim Khrypunov <khrypunovmax@gmail.com>
2025-05-17 19:09:14 +0300

    add views fix

--- shop/views.py
+++ shop/views.py
@@ -1,27 +1,36 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, mixins
 from rest_framework.pagination import PageNumberPagination
 
 from shop.models import Goods, Wine, Mood, Occasion
-from shop.serializers import GoodsSerializer, WineSerializer, MoodSerializer, OccasionSerializer
+from shop.serializers import (GoodsSerializer, WineSerializer,
+                              MoodSerializer, OccasionSerializer)
 
 
-class GoodsViewSet(viewsets.ModelViewSet):
+class GoodsViewSet(mixins.ListModelMixin,
+                   mixins.CreateModelMixin,
+                   viewsets.GenericViewSet):
     queryset = Goods.objects.all()
     serializer_class =GoodsSerializer
 
 
-class WineViewSet(viewsets.ModelViewSet):
+class WineViewSet(mixins.ListModelMixin,
+                   mixins.CreateModelMixin,
+                   viewsets.GenericViewSet):
     queryset = Wine.objects.all()
     serializer_class = WineSerializer
 
 
-class MoodleViewSet(viewsets.ModelViewSet):
+class MoodleViewSet(mixins.ListModelMixin,
+                   mixins.CreateModelMixin,
+                   viewsets.GenericViewSet):
     queryset = Mood.objects.all()
     serializer_class = MoodSerializer
 
 
-class OccasionViewSet(viewsets.ModelViewSet):
+class OccasionViewSet(mixins.ListModelMixin,
+                   mixins.CreateModelMixin,
+                   viewsets.GenericViewSet):
     queryset = Occasion.objects.all()
     serializer_class = OccasionSerializer
 
