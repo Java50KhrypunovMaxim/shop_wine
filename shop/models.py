a7fcd312cca9c2034286424a2227e52d0cc9f0ba
Maxim Khrypunov <khrypunovmax@gmail.com>
2025-05-17 17:53:10 +0300

    add wine models fix

--- shop/models.py
+++ shop/models.py
@@ -14,23 +14,37 @@ class WineColor(models.TextChoices):
     ROSE = 'rose'
 
 
+class OccasionType(models.TextChoices):
+    GIFT = 'gift'
+    ROMANTIC_DINNER = 'romantic_dinner'
+    CELEBRATION = 'celebration'
+
+
+class MoodType(models.TextChoices):
+    ROMANTIC = 'romantic'
+    FESTIVE = 'festive'
+    CASUAL = 'casual'
+
+
 class Occasion(models.Model):
-    name = models.CharField(max_length=50, choices=[
-        'gift',
-        'romantic_dinner',
-        'celebration',
-    ], unique=True)
+    name = models.CharField(
+        max_length=50,
+        choices=OccasionType.choices,
+        default=OccasionType.ROMANTIC_DINNER,
+        unique=True
+    )
 
     def __str__(self):
         return self.get_name_display()
 
 
 class Mood(models.Model):
-    name = models.CharField(max_length=50, choices=[
-        'romantic',
-        'festive',
-        'casual',
-    ], unique=True)
+    name = models.CharField(
+        max_length=50,
+        choices=MoodType.choices,
+        default=MoodType.FESTIVE,
+        unique=True
+    )
 
     def __str__(self):
         return self.get_name_display()
@@ -43,15 +57,18 @@ class PriceRange(models.TextChoices):
 
 
 class Wine(models.Model):
-    name = models.CharField(max_length=200)
-    wine_type = models.CharField(max_length=20, choices=WineType.choices)
-    color = models.CharField(max_length=20, choices=WineColor.choices)
-    country = models.CharField(max_length=200)
-    producer = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, null=True)
+    wine_type = models.CharField(max_length=20, choices=WineType.choices,
+                                 default=WineType.DESSERT)
+    color = models.CharField(max_length=20, choices=WineColor.choices,
+                             default=WineColor.RED)
+    country = models.CharField(max_length=200, null=True, blank=True)
+    producer = models.CharField(max_length=200, null=True, blank=True)
     occasions = models.ManyToManyField(Occasion, blank=True)
     moods = models.ManyToManyField(Mood, blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
-    price_range = models.CharField(max_length=20, choices=PriceRange.choices)
+    price_range = models.CharField(max_length=20, choices=PriceRange.choices,
+                                   default=PriceRange.BUDGET)
     description = models.TextField(blank=True)
 
     class Meta:
