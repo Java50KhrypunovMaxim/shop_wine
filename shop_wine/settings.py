522e8ac3753a4754219f54cca555790e8e0c2674
Maxim Khrypunov <khrypunovmax@gmail.com>
2025-05-17 19:59:26 +0300

    add urls

--- shop_wine/settings.py
+++ shop_wine/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'drf_spectacular',
     'rest_framework',
     'shop',
     'user',
@@ -126,3 +127,16 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 3,
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'SHOP OF WINE API',
+    'DESCRIPTION': 'Documentation for SHOP OF WINE API',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
+}
