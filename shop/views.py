d6a10d74938e2819ae202db3488e64d33e9cb8d8
Maxim Khrypunov <khrypunovmax@gmail.com>
2025-05-17 19:07:25 +0300

    add views

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.pagination import PageNumberPagination
 
-# Create your views here.
+from shop.models import Goods, Wine, Mood, Occasion
+from shop.serializers import GoodsSerializer, WineSerializer, MoodSerializer, OccasionSerializer
+
+
+class GoodsViewSet(viewsets.ModelViewSet):
+    queryset = Goods.objects.all()
+    serializer_class =GoodsSerializer
+
+
+class WineViewSet(viewsets.ModelViewSet):
+    queryset = Wine.objects.all()
+    serializer_class = WineSerializer
+
+
+class MoodleViewSet(viewsets.ModelViewSet):
+    queryset = Mood.objects.all()
+    serializer_class = MoodSerializer
+
+
+class OccasionViewSet(viewsets.ModelViewSet):
+    queryset = Occasion.objects.all()
+    serializer_class = OccasionSerializer
+
+
+class OrderPagination(PageNumberPagination):
+    page_size = 10
+    max_page_size = 100
