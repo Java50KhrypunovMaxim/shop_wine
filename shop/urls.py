04483b493d68bd7ee5e5b17e1036385c8205b0dc
Maxim Khrypunov <khrypunovmax@gmail.com>
2025-05-18 20:35:13 +0300

    add urls

--- shop/urls.py
+++ shop/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from rest_framework import routers
-from shop.views import GoodsViewSet, WineViewSet, MoodleViewSet
+from shop.views import GoodsViewSet, WineViewSet, MoodleViewSet, CountryViewSet, ProducerViewSet, GlassViewSet, \
+    CorkscrewViewSet
 
 app_name = "shop"
 
@@ -8,6 +9,10 @@ router = routers.DefaultRouter()
 router.register("goods", GoodsViewSet)
 router.register("wine", WineViewSet)
 router.register("mood", MoodleViewSet)
+router.register("country", CountryViewSet)
+router.register("producer", ProducerViewSet)
+router.register("glass", GlassViewSet)
+router.register("corkscrew", CorkscrewViewSet)
 
 
 urlpatterns = [path("", include(router.urls))]
