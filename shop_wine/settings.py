fad46237ecfab55044ef4e1f45996d37d23f29e7
Maxim Khrypunov <khrypunovmax@gmail.com>
2025-06-03 16:16:37 +0300

    fix cors problem

--- shop_wine/settings.py
+++ shop_wine/settings.py
@@ -27,6 +27,12 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
+CORS_ALLOW_ALL_ORIGINS = True
+
+#CORS_ALLOWED_ORIGINS = [
+#    "http://localhost:3000",
+#]
+
 
 # Application definition
 
@@ -38,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'import_export',
+    'corsheaders',
     'drf_spectacular',
     'rest_framework',
     'shop',
@@ -48,6 +55,7 @@ INSTALLED_APPS = [
 AUTH_USER_MODEL = 'user.User'
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
